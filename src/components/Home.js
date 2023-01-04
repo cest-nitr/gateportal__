aeba2753ab380e44a5b4147a0a069c0248d21162
Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>
2023-01-04 10:42:25 +0530

    home was added
    Signed-off-by: Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import Banner from './Banner'
+import About from './About'
+import Footer from './Footer'
 const Home = () => {
   return (
-    <div>
-      HOME
-    </div>
+    <>
+    <Banner />
+    <About />
+    <Footer />
+    </>
   )
 }
 

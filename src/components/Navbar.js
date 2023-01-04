d67f436b75b8efd003254a956af0b46ae4174f7e
Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>
2023-01-04 10:30:23 +0530

    updated navbar
    Signed-off-by: Sakalabhaktula Hanisha <87903473+Sakalabhaktula-Hanisha@users.noreply.github.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import logo from '../images/CEST White Logo.png'
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+    const [registered, setRegistered] = useState(true);
   return (
-    <>
-    <div className='shadow-md w-full fixed top-0 left-0'>
-        <div className="md:flex items-center justify-between py-4 md:px-10 px-7">
-            <div className='font-bold text-2xl cursor-pointer flex items-center'>
-                CEST
-            </div>
-            <ul className='md:flex md:items-center'>
-                <Link className='md:ml-8 text-xl' to='/' >Home</Link>
-                <Link className='md:ml-8 text-xl' to='/profile' >Profile</Link>
-                <Link className='md:ml-8 text-xl' to='/contact' >Contact</Link>
-                <Link className='md:ml-8 text-xl' to='/login' >Login</Link>
-                <Link className='md:ml-8 text-xl' to='/signup' >Sign Up</Link>
-            </ul>
+    <div className='bg-[#2C74B3] p-2 text-white'>
+        <div className='max-w-[1240px] items-center py-2 mx-auto flex justify-between'>
+        <div className='text-3xl font-bold'>
+            <img className='w-12' src={logo}></img>
+        </div>
+        {
+            toggle ? 
+            <AiOutlineClose onClick={() => setToggle(!toggle)} className='my-1 font-bold text-2xl cursor-pointer md:hidden'/>
+            :
+            <AiOutlineMenu onClick={() => setToggle(!toggle)} className='my-1 font-bold text-2xl cursor-pointer md:hidden'/>
+        }
+        {/* responsive nav */}
+        <ul className={`font-bold fixed bg-[#2C74B3] text-white w-screen left-0 top-0 py-2 text-xl flex-row justify-center items-center duration-500 z-[1] ${toggle ? 'top-[81px]' : 'top-[-100%]'}`}>
+            <li className='my-1 text-center'><Link to='/'>Home</Link></li>
+            <li className='my-1 text-center'><Link to='/'>About</Link></li>
+            <li className='my-1 text-center'><Link to='/profile'>Profile</Link></li> 
+            <li className='my-1 text-center'><Link to='/Contest'>Contest</Link></li>
+        </ul>
+        {/* responsive nav  */}
+        <ul className='hidden md:flex font-bold text-white gap-5'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'>About</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
+            <li><Link to='/Contest'>Contest</Link></li>
+        </ul>
+        <ul className='flex font-bold gap-5'>
+            {
+                registered ? 
+                <li><button onClick={() => setRegistered(!registered)} className='bg-white text-[#2C74B3] px-2 p-1 shadow-md rounded'><Link to='/logout'>Log out</Link></button></li>
+                :
+                <>
+                <li><button className='bg-white text-[#2C74B3] px-2 p-1 shadow-md rounded'><Link to='/login'>Login</Link></button></li>
+                <li><button className='bg-white text-[#2C74B3] px-2 p-1 shadow-md rounded'><Link to='/signin'>Sign in</Link></button></li>
+                </>
+            }
+        </ul>
         </div>
     </div>
-    </>
   )
 }
 
